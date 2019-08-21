3597b3378f8c95ef75b8d1d60533f5b4394a85d7
Rémi Bouguermouh <remibouguermouh@gmail.com>
2019-08-21 22:00:19 +0200

    Init Svelte app

---
+++ time-bomb-front-app/src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+	import Login from './login/Login.svelte';
+	import Nav from './Nav.svelte';
+
+	export let name;
+</script>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
+
+<h1>Hello {name}!</h1>
+
+<Nav />
+<Login />
---
+++ time-bomb-front-app/src/Nav.svelte
@@ -0,0 +1,11 @@
+<script>
+    // your script goes here
+</script>
+
+<style>
+    /* your styles go here */
+</style>
+
+<nav>
+    <a href="/login">Login</a>
+</nav>
